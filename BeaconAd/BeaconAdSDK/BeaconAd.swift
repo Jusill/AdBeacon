15430b0295d6e71a51df3b884bab2c38114d97f4
Jusill <Jusill98@gmail.com>
2017-08-25 17:46:18 +0000

    Init

---
+++ BeaconAd/BeaconAdSDK/BeaconAd.swift
@@ -0,0 +1,60 @@
+﻿//
+//  BeaconAd.swift
+//  BeaconAdSDK
+//
+//  Created by Boris Konstantinovskiy on 18.04.17.
+//  Copyright © 2017 BeaconAd. All rights reserved.
+//
+
+import Foundation
+import ZXingObjC
+
+public class BeaconAd {    
+    internal let UID: String
+    var beaconsUUID: [String] = []
+    var status = ["status": false, "message": "No beacons"]
+    
+    public init(companyUID: String, completion: (result: Bool) -> ()) {
+        UID = companyUID
+        self.GetBeacons(completion)
+    }
+    
+    public func getStatus() -> [String: AnyObject] {
+        return status
+    }
+    
+    func GetBeacons(completion: (result: Bool) -> ()) {
+        WebRequest.GetBeacons(UID) { (data, response, err) in
+            if (err != nil) {
+                self.status["message"] = "server error"
+                completion(result: false)
+            }
+            else {
+                var jsonData = NSDictionary()
+                do {
+                    jsonData = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as! NSDictionary
+                } catch
+                {
+                    self.status["message"] = "Ad UID error"
+                    completion(result: false)
+                }
+                
+                var dict: [String: AnyObject] = jsonData as! [String: AnyObject]
+                
+                if dict["status"] as! String == "error" {
+                    self.status["message"] = dict["mes"] as! String
+                    completion(result: false)
+                }
+                else {
+                    self.status["status"] = true
+                    self.status["message"] = "OK"
+                    
+                    self.beaconsUUID = dict["beacons"] as! [String]
+                    completion(result: true)
+                }
+                
+            }
+        }
+    }
+}
+
---
+++ BeaconAd/BeaconAdSDK/WebRequest.swift
@@ -0,0 +1,54 @@
+﻿//
+//  WebRequest.swift
+//  BeaconAdSDK
+//
+//  Created by Boris Konstantinovskiy on 18.04.17.
+//  Copyright © 2017 BeaconAd. All rights reserved.
+//
+
+import Foundation
+
+class WebRequest {
+    static let url = "http://flask-env.ubjap2jffs.us-east-1.elasticbeanstalk.com/api/"
+    
+    static let getbeacons = "getbeacons/"
+    static let getclientuid = "getclientuid"
+    static let getad = "getad"
+    
+    static func makePostRequest(path: String, body: String, completion: (data: NSData?, response: NSURLResponse?, err: NSError?) -> Void) {
+        let session = NSURLSession.sharedSession()
+        let URL = NSURL(string: url + path)
+        let request = NSMutableURLRequest(URL: URL!)
+        
+        request.HTTPMethod = "POST"
+        request.HTTPBody = body.dataUsingEncoding(NSUTF8StringEncoding)
+        
+        let task = session.dataTaskWithRequest(request, completionHandler: completion)
+        task.resume()
+    }
+    
+    static func makeGetRequest(path: String, completion: (data: NSData?, response: NSURLResponse?, err: NSError?) -> Void) {
+        let session = NSURLSession.sharedSession()
+        let URL = NSURL(string: url + path)
+        
+        let request = NSMutableURLRequest(URL: URL!)
+        request.HTTPMethod = "GET"
+        let task = session.dataTaskWithRequest(request, completionHandler: completion)
+        task.resume()
+    }
+    
+    static internal func GetBeacons(uid: String, completion: (data: NSData?, response: NSURLResponse?, err: NSError?) -> Void) {
+        makeGetRequest(getbeacons + uid, completion: completion)
+    }
+    
+    static internal func GetClientUID(email: String, completion: (data: NSData?, response: NSURLResponse?, err: NSError?) -> Void) {
+        let params = "?email=" + email
+        makeGetRequest(getclientuid + params, completion: completion)
+    }
+    
+    static internal func GetAd(client_uid: String, adcompany_uid: String, completion: (data: NSData?, response: NSURLResponse?, err: NSError?) -> Void) {
+        let postString = "user_uid=\(client_uid)&adcompany_uid=\(adcompany_uid)"
+        
+        makePostRequest(getad, body: postString, completion: completion)
+    }
+}
